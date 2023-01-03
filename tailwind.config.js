5061662b16b71ccc30c985a06e6455c0d7f0cfde
venkat <subash.venkat007@gmail.com>
2023-01-03 01:28:27 +0000

    removed tailwind css cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html",
-            "./src/**.*.vue"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
